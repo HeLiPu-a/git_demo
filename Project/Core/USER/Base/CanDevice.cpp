d107931f71558360e1839d1f9c63b848d602affc
Turing <ay52159wyh@163.com>
2024-11-15 22:10:49 +0800

    完成了CanDevice的编写

---
+++ Project/Core/USER/Base/CanDevice.cpp
@@ -0,0 +1,124 @@
+#include "CanDevice.h"
+//静态变量的初始化
+CanDevice* CanDevice::Can1_Instances[CAN_MAX_INSTANCES] = {nullptr};
+CanDevice* CanDevice::Can2_Instances[CAN_MAX_INSTANCES] = {nullptr};
+uint8_t CanDevice::Can1_Instances_Index = 0;
+uint8_t CanDevice::Can2_Instances_Index = 0;
+uint8_t CanDevice::RxData1[8] = {0};
+uint8_t CanDevice::RxData2[8] = {0};
+
+CanDevice::CanDevice(CAN_HandleTypeDef *hcan_, uint32_t can_id_):hcan(hcan_), can_id(can_id_){
+    if(hcan == &hcan1){
+        if(Can1_Instances_Index < CAN_MAX_INSTANCES){       //将can1上的所有实例指针存入数组
+            uint8_t i;
+            for(i=0;i <= CanDevice::Can1_Instances_Index;i++){      //遍历数组查找是否有can_id相同的实例，有则跳出循环
+			    if(Can1_Instances[i]->can_id == can_id_ || Can1_Instances[i] == nullptr){
+					break;
+				}
+			}
+			if(i == CanDevice::Can1_Instances_Index){    //遍历完如果i等于CanDevice::Can1_Instances_Index，则说明没有找到相同的实例，则存入数组
+				Can1_Instances[Can1_Instances_Index] = this;
+				Can1_Instances_Index++;
+			}
+        }
+    }else if(hcan_ == &hcan2){
+        if(Can2_Instances_Index < CAN_MAX_INSTANCES){       //将can2上的所有实例指针存入数组
+            uint8_t i;
+            for(i=0;i <= CanDevice::Can2_Instances_Index;i++){      //遍历数组查找是否有can_id相同的实例，有则跳出循环
+			    if(Can2_Instances[i]->can_id == can_id_ || Can2_Instances[i] == nullptr){
+					break;
+				}
+			}
+			if(i == CanDevice::Can2_Instances_Index){    //遍历完如果i等于CanDevice::Can2_Instances_Index，则说明没有找到相同的实例，则存入数组
+				Can2_Instances[Can2_Instances_Index] = this;
+				Can2_Instances_Index++;
+			}
+        }
+    }
+}
+
+void CanDevice::Can_Init(){
+	CAN1_Filter_Init();
+	CAN2_Filter_Init();
+}
+
+void CanDevice::CAN1_Filter_Init(){
+   CAN_FilterTypeDef sFilterconfig;
+	sFilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;  //32位过滤器
+	sFilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;  //掩码模式
+	sFilterconfig.FilterIdHigh = 0;
+	sFilterconfig.FilterIdLow = 0;
+	sFilterconfig.FilterMaskIdHigh = 0;    //不过滤任何ID号，即接收所有ID号的报文
+	sFilterconfig.FilterMaskIdLow = 0;
+	sFilterconfig.FilterBank = 0;
+	sFilterconfig.FilterActivation = CAN_FILTER_ENABLE;   //启用过滤器
+	sFilterconfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;  //通过过滤器的报文放置FIFO0
+	if(HAL_OK != HAL_CAN_ConfigFilter(&hcan1,&sFilterconfig)){
+		Error_Handler();
+	}
+
+	if(HAL_OK != HAL_CAN_Start(&hcan1)){   //启动can
+		Error_Handler();
+	}
+	
+	if(HAL_OK != HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING)){  //使能fifo0接收中断
+		Error_Handler();
+	}
+}
+
+void CanDevice::CAN2_Filter_Init(){
+
+    CAN_FilterTypeDef sFilterconfig;
+	sFilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;  //32位过滤器
+	sFilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;  //掩码模式
+	sFilterconfig.FilterIdHigh = 0;
+	sFilterconfig.FilterIdLow = 0;
+	sFilterconfig.FilterMaskIdHigh = 0;    //不过滤任何ID号，即接收所有ID号的报文
+	sFilterconfig.FilterMaskIdLow = 0;
+	sFilterconfig.FilterBank = 14;
+	sFilterconfig.FilterActivation = CAN_FILTER_ENABLE;   //启用过滤器
+	sFilterconfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;  //通过过滤器的报文放置FIFO0
+    sFilterconfig.SlaveStartFilterBank = 14;  //can2的过滤器从14号开始,避免与can1的过滤器冲突
+	if(HAL_OK != HAL_CAN_ConfigFilter(&hcan2, &sFilterconfig)){
+		Error_Handler();
+	}
+	if(HAL_OK != HAL_CAN_Start(&hcan2)){   //启动can
+		Error_Handler();
+	}
+	
+	if(HAL_OK != HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO0_MSG_PENDING)){  //使能fifo0接收中断
+		Error_Handler();
+	}
+}
+
+void CanDevice::Can_update(uint8_t can_RxData[8],CAN_RxHeaderTypeDef* RxHeader){
+    //具体实现在子类中，不写成纯虚函数是为了兼容不使用该函数的CAN设备
+}
+
+void CanDevice::Can_SendData(){
+    //具体实现在子类中，不写成纯虚函数是为了兼容不使用该函数的CAN设备
+}
+
+extern "C" void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
+    if(hcan == &hcan1){
+        CAN_RxHeaderTypeDef RxHeader1;
+        if(HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &RxHeader1, CanDevice::RxData1) != HAL_OK){
+            Error_Handler();
+        }
+        for(uint8_t i = 0; i < CanDevice::Can1_Instances_Index; i++){
+            if(CanDevice::Can1_Instances[i] != nullptr){
+                CanDevice::Can1_Instances[i]->Can_update(CanDevice::RxData1, &RxHeader1);    //调用所有实例中的Can_update函数，如何识别是否是该实例所需的报文，由子类实现，比如判断stdid等
+            }
+        }
+    }else if(hcan == &hcan2){
+        CAN_RxHeaderTypeDef RxHeader2;
+        if(HAL_CAN_GetRxMessage(&hcan2, CAN_RX_FIFO0, &RxHeader2, CanDevice::RxData2) != HAL_OK){
+            Error_Handler();
+        }
+        for(uint8_t i = 0; i < CanDevice::Can2_Instances_Index; i++){
+            if(CanDevice::Can2_Instances[i] != nullptr){
+                CanDevice::Can2_Instances[i]->Can_update(CanDevice::RxData2, &RxHeader2);
+            }
+        }
+    }
+}
---
+++ Project/Core/USER/Base/CanDevice.h
@@ -0,0 +1,52 @@
+#ifndef CAN_DEVICE_H
+#define CAN_DEVICE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*在此处引用外部文件：       begin*/	
+#include "stm32f4xx_hal.h"
+#include "can.h"
+#include <stdint.h>
+/*引用外部文件end*/	
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+#define CAN_MAX_INSTANCES 10 //暂时设置一条can设备最多可以创建10个实例,后面有需要可以再修改
+
+class CanDevice
+{
+public: 
+    CanDevice(CAN_HandleTypeDef *hcan_, uint32_t can_id_);
+
+    CAN_HandleTypeDef *hcan = nullptr;  // 保存CAN 句柄
+    CAN_TxHeaderTypeDef TxHeader1,TxHeader2;  // CAN下发报文的数据头,在子类中根据需要配置
+
+    uint32_t can_id = 0;  //保存每个实例的can_id
+    uint8_t send_buf1[8] = {0};  // CAN1下发报文的数据
+    uint8_t send_buf2[8] = {0};  // CAN2下发报文的数据
+    uint32_t msg_box1 = 0;  //保存can1下发报文使用的邮箱
+    uint32_t msg_box2 = 0;  //保存can2下发报文使用的邮箱
+    
+    static CanDevice *Can1_Instances[CAN_MAX_INSTANCES];  // 保存can1上的所有实例
+	static uint8_t Can1_Instances_Index;  // can1实例的数量
+    static CanDevice *Can2_Instances[CAN_MAX_INSTANCES];  // 保存can2上的所有实例
+	static uint8_t Can2_Instances_Index;  // can2实例的数量
+    static uint8_t RxData1[8];  //存储CAN1接收到的数据
+    static uint8_t RxData2[8];  //存储CAN2接收到的数据
+    
+    virtual void Can_SendData();  //CAN发送报文函数
+    virtual void Can_update(uint8_t can_RxData[8],CAN_RxHeaderTypeDef* RxHeader);  //用于更新c6020电调上传的数据，其他设备有类似操作也可以重写该函数
+    static void CAN1_Filter_Init(void);  //can过滤器配置函数
+    static void CAN2_Filter_Init(void);  //默认接收所有can数据帧，使用32位过滤器掩码模式
+	static void Can_Init();  //初始化过滤器，整个工程只需要调用一次即可
+};
+
+#endif
+
+#endif
